25ea7dd4a4d3b3ec91ce0927d98641578c5d10f0
zkoziel <zkoziel@gmail.com>
2021-06-11 08:49:45 +0200

    initialcommit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+ 
+namespace MyUltimateSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine("Hi Pawel");
+            for(int i=0;i<10;i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine(rand.Next(100,999));
+            }
+        }
+    }
+}
